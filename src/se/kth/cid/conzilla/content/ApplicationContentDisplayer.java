e3918f7ed8ae992d1e61a96bd65de6ed6ae17cd4
Hannes Ebner <hannes.ebner@gmail.com>
2008-03-25 10:21:30 +0000

    fixes the URL opening issue on newer Macs

--- src/se/kth/cid/conzilla/content/ApplicationContentDisplayer.java
+++ src/se/kth/cid/conzilla/content/ApplicationContentDisplayer.java
@@ -5,78 +5,70 @@
  */
 
 package se.kth.cid.conzilla.content;
+
 import java.net.URL;
 
-public class ApplicationContentDisplayer extends BrowserContentDisplayer
-{  
-  protected boolean showDocument(URL uri) throws ContentException
-    {
-               try {
-                   String os = (String) System.getProperties().get("os.name");
-                   String [] command = null;
-                   if (os.toLowerCase().matches(".*windows.*")) {
-                       command = new String[3];
-                       command[0] = "rundll32";
-                       command[1] = "url.dll,FileProtocolHandler";
-                       command[2] = uri.toString();
-                   } else if (os.toLowerCase().matches(".*mac.*")) {
-                       command = new String[2];
-                       command[0] = "open";
-                       command[1] = uri.toString();
-                   } else if (os.toLowerCase().matches(".*linux.*")) {
-                       //command = new String[3];
-                       //command[0] = "netscape";
-                       //command[1] = "-remote";
-                       //command[2] = "openURL(" + uri.toString() + ")";
-                       Process p = Runtime.getRuntime().exec("mozilla -remote ping()");
-                       int ping = p.waitFor();
-                       if (ping == 0) {
-                           command = new String[3];
-                           command[0] = "mozilla";
-                           command[1] = "-remote";
-                           command[2] = "openURL("+uri+")";
-                       } else {
-                           command = new String[2];
-                           command[0] = "mozilla";
-                           command[1] = uri.toString();
-                       }
-                   }
-                   if (command != null) {
-                       Runtime.getRuntime().exec(command);
-                   }
-               } catch (Exception e) {
-                   e.printStackTrace();
-                   return false;
-               }
-               return true;
-        
-/*      String [] command;
-      //      if (com.apple.mrj.MRJApplicationUtils.isMRJToolkitAvailable())
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-      //Mac support.
-      //if (System.getProperty("mrj.version") != null)
-      //    com.apple.mrj.MRJFileUtils.openURL(url.toString());
-      //else 
-      if (File.separatorChar == '/')
-	{
-	  command = new String[3];
-	  command[0] = "";
-	  command[1] = "-remote";
-	  command[2] = "openURL(" + url.toString() + ")";
+import edu.stanford.ejalbert.BrowserLauncher;
+
+public class ApplicationContentDisplayer extends BrowserContentDisplayer {
+	
+	Log log = LogFactory.getLog(ApplicationContentDisplayer.class);
+	
+	protected boolean showDocument(URL uri) throws ContentException {
+		BrowserLauncher launcher;
+		try {
+			launcher = new BrowserLauncher();
+			launcher.openURLinBrowser(uri.toString());
+		} catch (Exception browsere) {
+			log.warn(browsere.getMessage());
+			
+			try {
+				String os = (String) System.getProperties().get("os.name");
+				String[] command = null;
+				if (os != null) {
+					if (os.toLowerCase().matches(".*windows.*")) {
+						command = new String[3];
+						command[0] = "rundll32";
+						command[1] = "url.dll,FileProtocolHandler";
+						command[2] = uri.toString();
+					} else if (os.toLowerCase().matches(".*mac.*")) {
+						command = new String[2];
+						command[0] = "open";
+						command[1] = uri.toString();
+					} else if (os.toLowerCase().matches(".*linux.*")) {
+						Process p = Runtime.getRuntime().exec("mozilla -remote ping()");
+						int ping = p.waitFor();
+						if (ping == 0) {
+							command = new String[3];
+							command[0] = "mozilla";
+							command[1] = "-remote";
+							command[2] = "openURL(" + uri + ")";
+						} else {
+							command = new String[2];
+							command[0] = "mozilla";
+							command[1] = uri.toString();
+						}
+					}
+				} else {
+					// we tend to get "null" for os.name on newer mac systems,
+					// so we try it the mac way here
+					command = new String[2];
+					command[0] = "open";
+					command[1] = uri.toString();
+				}
+				if (command != null) {
+					Runtime.getRuntime().exec(command);
+				}
+			} catch (Exception e) {
+				log.error(e.getMessage(), e);
+				return false;
+			}
+		}
+		
+		return true;
 	}
-      else
-	{
-	  command=new String[2];
-	  command[0] = "browser.bat";
-	  command[1] = url.toString();
-	}
-      try {
-	Runtime.getRuntime().exec(command);
-      } catch(IOException e)
-	{
-	  throw new ContentException("Could not execute browser:\n "
-				     + e.getMessage(), null);
-	}
-      return true;*/
-    }
+
 }
